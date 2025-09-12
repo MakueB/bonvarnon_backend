5983a61b6629dadca4050d9af4b66ed39aed6033
MakueB <makbones@gmail.com>
2025-09-12 20:49:20 +0300

    добавил интерфейс OrderService.kt

--- src/main/kotlin/domain/api/OrderService.kt
+++ src/main/kotlin/domain/api/OrderService.kt
@@ -1,4 +1,10 @@
 package ru.makiev.domain.api
 
+import ru.makiev.application.dto.order.OrderResponse
+
 interface OrderService {
+    suspend fun createOrder(): OrderResponse
+    suspend fun updateOrderStatus()
+    suspend fun getOrderById(orderId: Int): OrderResponse?
+    suspend fun getOrdersByUser(userId: Int): List<OrderResponse>
 }
