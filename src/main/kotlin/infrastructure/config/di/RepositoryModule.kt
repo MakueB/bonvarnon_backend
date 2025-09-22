d46af15a4be075e947c77a1d6d45214cefe9054a
MakueB <makbones@gmail.com>
2025-09-22 13:44:56 +0300

    добавил репозитории и сервисы в di

--- src/main/kotlin/infrastructure/config/di/RepositoryModule.kt
+++ src/main/kotlin/infrastructure/config/di/RepositoryModule.kt
@@ -6,8 +6,12 @@ import ru.makiev.data.repository.MenuItemRepositoryImpl
 import ru.makiev.domain.api.AdditiveRepository
 import ru.makiev.domain.api.MenuCategoryRepository
 import ru.makiev.domain.api.MenuItemRepository
+import ru.makiev.domain.api.OrderRepostory
+import ru.makiev.domain.api.SizeRepository
 import ru.makiev.domain.api.UserRepository
 import ru.makiev.infrastructure.repository.AdditiveRepositoryImpl
+import ru.makiev.infrastructure.repository.OrderRepositoryImpl
+import ru.makiev.infrastructure.repository.SizeRepositoryImpl
 import ru.makiev.infrastructure.repository.UserRepositoryImpl
 
 val repositoryModule = module {
@@ -25,5 +29,17 @@ val repositoryModule = module {
 
     single<UserRepository> {
         UserRepositoryImpl()
+    }
+
+    single<OrderRepostory> {
+        OrderRepositoryImpl()
+    }
+
+    single<AdditiveRepository> {
+        AdditiveRepositoryImpl()
+    }
+
+    single<SizeRepository> {
+        SizeRepositoryImpl()
     }
 }
--- src/main/kotlin/infrastructure/config/di/ServiceModule.kt
+++ src/main/kotlin/infrastructure/config/di/ServiceModule.kt
@@ -3,9 +3,11 @@ package ru.makiev.infrastructure.config.di
 import org.koin.dsl.module
 import ru.makiev.domain.api.AdditiveService
 import ru.makiev.domain.api.MenuService
+import ru.makiev.domain.api.OrderService
 import ru.makiev.domain.api.UserService
 import ru.makiev.domain.service.AdditiveServiceImpl
 import ru.makiev.domain.service.MenuServiceImpl
+import ru.makiev.domain.service.OrderServiceImpl
 import ru.makiev.domain.service.UserSeviceImpl
 
 val serviceModule = module {
@@ -19,5 +21,9 @@ val serviceModule = module {
 
     single<UserService> {
         UserSeviceImpl(get(),get())
+    }
+
+    single<OrderService> {
+        OrderServiceImpl(get(),get(),get(),get(),get())
     }
 }
